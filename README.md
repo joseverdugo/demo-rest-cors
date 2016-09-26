0477f5da1fec58b5be4ee73f76cc91fbc50f11f7
joseverdugo <jose.verdugo@tinet.cl>
2016-09-26 12:44:17 -0300

    Demo

---
+++ README.md
@@ -0,0 +1,10 @@
+
+Referencia:
+https://github.com/spring-guides/gs-rest-service-cors
+
+Ejecutar:
+mvn spring-boot:run -Dserver.port=9000
+mvn spring-boot:run -Dserver.port=8080
+
+Acceeder a:
+localhost:9000
---
+++ public/hello.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $.ajax({
+        url: "http://localhost:8080/greeting"
+    }).then(function(data, status, jqxhr) {
+       $('.greeting-id').append(data.id);
+       $('.greeting-content').append(data.content);
+       console.log(jqxhr);
+    });
+    
+    
+    $.ajax({
+        url: "http://localhost:8080/greeting-javaconfig"
+    }).then(function(data, status, jqxhr) {
+       $('.greeting-id-2').append(data.id);
+       $('.greeting-content-2').append(data.content);
+       console.log(jqxhr);
+    });
+});
